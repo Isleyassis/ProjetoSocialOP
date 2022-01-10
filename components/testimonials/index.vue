5215c2e45784e56d79dd6a6f18438250d3909989
Isley Assis <isleyassis@gmail.com>
2022-01-10 14:32:36 -0300

    :rocket: adicionando logica nos depoimentos

--- components/testimonials/index.vue
+++ components/testimonials/index.vue
@@ -3,94 +3,21 @@
         <div class="main-testimonials__title">
             <h3>Depoimentos</h3>
         </div>
-        <b-row cols="2">
-            <b-col>
+
+        <b-row cols="2">   
+            <b-col v-for="(test, index) in testimonials" :key="index">
                 <div class="main-testimonials__box">
                     <div class="main-testimonials__wrapper-infos">
                         <div class="main-testimonials__wrapper-img">
+                            <img :src="test.imgURL" alt="">
                         </div>
-                        <div class="main-testimonials__wrapper-name">
-                            <h5 style="margin: 0;">Nome do Aluno</h5>
-                            <small>Curso</small>
-                        </div>
+                            <div class="main-testimonials__wrapper-name">
+                                <h5> {{ test.nameStudent }} </h5>
+                                <small> {{ test.course }} </small>
+                            </div>
                     </div>
                     <div class="main-testimonials__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.</p>
-                    </div>
-                </div>
-            </b-col>
-             <b-col>
-                <div class="main-testimonials__box">
-                    <div class="main-testimonials__wrapper-infos">
-                        <div class="main-testimonials__wrapper-img">
-                        </div>
-                        <div class="main-testimonials__wrapper-name">
-                            <h5 style="margin: 0;">Nome do Aluno</h5>
-                            <small>Curso</small>
-                        </div>
-                    </div>
-                    <div class="main-testimonials__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.</p>
-                    </div>
-                </div>
-            </b-col>
-             <b-col>
-                <div class="main-testimonials__box">
-                    <div class="main-testimonials__wrapper-infos">
-                        <div class="main-testimonials__wrapper-img">
-                        </div>
-                        <div class="main-testimonials__wrapper-name">
-                            <h5 style="margin: 0;">Nome do Aluno</h5>
-                            <small>Curso</small>
-                        </div>
-                    </div>
-                    <div class="main-testimonials__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.</p>
-                    </div>
-                </div>
-            </b-col>
-             <b-col>
-                <div class="main-testimonials__box">
-                    <div class="main-testimonials__wrapper-infos">
-                        <div class="main-testimonials__wrapper-img">
-                        </div>
-                        <div class="main-testimonials__wrapper-name">
-                            <h5 style="margin: 0;">Nome do Aluno</h5>
-                            <small>Curso</small>
-                        </div>
-                    </div>
-                    <div class="main-testimonials__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.</p>
-                    </div>
-                </div>
-            </b-col>
-             <b-col>
-                <div class="main-testimonials__box">
-                    <div class="main-testimonials__wrapper-infos">
-                        <div class="main-testimonials__wrapper-img">
-                        </div>
-                        <div class="main-testimonials__wrapper-name">
-                            <h5 style="margin: 0;">Nome do Aluno</h5>
-                            <small>Curso</small>
-                        </div>
-                    </div>
-                    <div class="main-testimonials__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.</p>
-                    </div>
-                </div>
-            </b-col>
-             <b-col>
-                <div class="main-testimonials__box">
-                    <div class="main-testimonials__wrapper-infos">
-                        <div class="main-testimonials__wrapper-img">
-                        </div>
-                        <div class="main-testimonials__wrapper-name">
-                            <h5 style="margin: 0;">Nome do Aluno</h5>
-                            <small>Curso</small>
-                        </div>
-                    </div>
-                    <div class="main-testimonials__text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.</p>
+                        <p> {{ test.text }} </p>
                     </div>
                 </div>
             </b-col>
@@ -98,8 +25,54 @@
     </b-container>
 </template>
 
-<style scoped>
+<script>
+export default {
+    data() {
+        return {
+            testimonials: [
+                {
+                    nameStudent: 'Isley Barbosa de Assis',
+                    course: 'informática',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.',
+                    imgURL: "/juliete.png"
+                },
+                {
+                    nameStudent: 'iago Rafael',
+                    course: 'informática',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.',
+                    imgURL: "/juliete.png"
+                },
+                {
+                    nameStudent: 'Edinardo Filho',
+                    course: 'informática',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.',
+                    imgURL: "/juliete.png"
+                },
+                {
+                    nameStudent: 'Kayque Viana',
+                    course: 'informática',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.',
+                    imgURL: "/juliete.png"
+                },
+                 {
+                    nameStudent: 'Natanael Lima',
+                    course: 'informática',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.',
+                    imgURL: "/juliete.png"
+                },
+                 {
+                    nameStudent: 'Ezequias Oliveira',
+                    course: 'informática',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ipsum gravida, condimentum nisl non, maximus risus.',
+                    imgURL: "/juliete.png"
+                },
+            ]
+        }
+    }
+}
+</script>
 
+<style scoped>
 .main-testimonials {
     margin-bottom: 50px;
 }
@@ -112,9 +85,13 @@
 }
 
 .main-testimonials__wrapper-img {
-    background-color: #C4C4C4;
     width: 59px;
     height: 54px;
+}
+
+.main-testimonials__wrapper-img img{
+    width: 100%;
+    height: 100%;
 }
 
 .main-testimonials__title {
@@ -138,7 +115,7 @@
     position: absolute;
     content: '';
     bottom: -10%;
-    left: 20%;
+    left: 25%;
     width: 100px;
     height: 4px;
     background-color: #22A800;
@@ -153,6 +130,9 @@
     display: flex;
     flex-direction: column;
     margin-left: 20px; 
+}
+.main-testimonials__wrapper-name h5{
+    margin-bottom: 10px; 
 }
 
 </style>
