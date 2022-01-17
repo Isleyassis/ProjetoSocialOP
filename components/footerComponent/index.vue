80abfb9909eb95a41890df220f3256ce5331dc01
Isley Assis <isleyassis@gmail.com>
2022-01-17 12:14:57 -0300

    :rocket: iniciando footer-mobile

--- components/footerComponent/index.vue
+++ components/footerComponent/index.vue
@@ -3,10 +3,12 @@
         <div class="main-footer__title">
             <h2>Informática 2019 - 2021</h2>
         </div>
-        <div class="main-footer__wrapper-img">
+
+        <div class="main-footer__background">
+            <div class="main-footer__wrapper-img">
             <img src="Foto Rodapé.jpeg" alt="">
         </div>
-        <div class="main-footer__background"></div>
+        </div>
         <div class="main-footer__nameproject">
             <p>
                 NOME DO PROJETO - 2021
@@ -20,27 +22,53 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        margin-bottom: 60px;
+        margin-bottom: 160px;
     }
 
     .main-footer__wrapper-img {
         position: absolute;
-        width: 60%;
-        height: 600px;
-        top: -30px;
+        width: 750px;
+        height: 400px;
+        top: -65px;
+        left: 25%;
+
+        @media(max-width: 600px) {
+            width: 90%;
+            height: 100px;
+            top: -65px;
+            left: 5%;
+        }
+
+        @media(max-width: 992px) {
+            height: 350px;
+        }
+
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
     }
 
     .main-footer__background {
         background-color: darkgreen;
-        height: 700px;
+        height: 500px;
         position: relative;
+
+        @media (max-width: 600px) {
+            height: 200px;
+        }
+
+        @media (max-width: 992px) {
+            height: 350px;
+        }
     }
 
     .main-footer__nameproject {
         background-color: lightgreen;
         display: flex;
         justify-content: flex-end;
-        padding: 5px 10%;
+        padding: 10px 10%;
 
         p {
             margin: 0;
